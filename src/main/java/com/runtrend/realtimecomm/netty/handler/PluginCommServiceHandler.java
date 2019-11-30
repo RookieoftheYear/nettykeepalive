b0800574eea2fd111a279e3d8fb1db00e89c6732
RookieoftheYear <5627009@qq.com>
2019-11-30 22:12:25 +0800

    删除一些注释

--- src/main/java/com/runtrend/realtimecomm/netty/handler/PluginCommServiceHandler.java
+++ src/main/java/com/runtrend/realtimecomm/netty/handler/PluginCommServiceHandler.java
@@ -42,7 +42,6 @@ public class PluginCommServiceHandler extends SimpleChannelInboundHandler<FullHt
 
         ctx.fireChannelActive();
 
-//        log.debug("{} is Connecting", ctx.channel().remoteAddress());
 
     }
 
